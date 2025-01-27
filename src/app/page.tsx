b6b8dd60ab8d7e7778a9fde08999c142510cdc62
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2025-01-27 09:30:39 -0800

    added main buttons and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
+export default function App() {
   return (
-    <div className="text-center font-extrabold text-5xl p-6">GYM GURU APP</div>
+    <main>
+      <div>
+        <h1 className="text-center font-extrabold text-8xl pt-10">GYM GURU</h1>
+        <p className="text-center font-bold text-xl p-2 text-yellow-500">
+          Your ultimate gym trainer
+        </p>
+      </div>
+      <div className="flex flex-col space-y-4 items-stretch justify-center max-w-lg mx-auto w-full py-4">
+        <button className="text-center bg-white border-2 text-black text-5xl shadow-lg rounded-md p-8 gap-4 hover:bg-yellow-400">
+          Explore Exercises
+        </button>
+        <button className="text-center bg-white border-2 text-black text-5xl shadow-lg rounded-md p-8 gap-4 hover:bg-yellow-400">
+          Saved Exercises
+        </button>
+        <button className="text-center bg-white border-2 text-black text-5xl shadow-lg rounded-md p-8 gap-4 hover:bg-yellow-400">
+          My Workout Plan
+        </button>
+      </div>
+    </main>
   );
 }
