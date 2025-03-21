ce7653ac980ef47cc2633e3fec019b3f46d9d7f7
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2025-03-20 18:06:00 -0700

    migrated backend to separate app

--- config/dev.js
+++
@@ -1,3 +0,0 @@
-// for dev
-export const MONGO_URI = 'mongodb://localhost:27017/gym-guru'
-export const TOKEN_SECRET = 'test_token_secret'
--- config/prod.js
+++
@@ -1,2 +0,0 @@
-export const MONGO_URI = process.env.MONGO_URI;
-export const TOKEN_SECRET = process.env.TOKEN_SECRET;
